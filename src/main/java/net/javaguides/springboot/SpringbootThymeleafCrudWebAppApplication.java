afe7bad46b5e4d5155b87aece50bd35c07bf308d
Phillips III, Ernest Alvin CTR USA <ernest.a.phillips4.ctr@mail.mil>
2021-09-01 08:18:25 -0400

    adding changes with encryption

--- src/main/java/net/javaguides/springboot/SpringbootThymeleafCrudWebAppApplication.java
+++ src/main/java/net/javaguides/springboot/SpringbootThymeleafCrudWebAppApplication.java
@@ -1,9 +1,12 @@
 package net.javaguides.springboot;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class SpringbootThymeleafCrudWebAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -1,11 +1,8 @@
 package net.javaguides.springboot.repository;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
 import net.javaguides.springboot.model.Employee;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
-@Repository
 public interface EmployeeRepository extends MongoRepository<Employee, Long>{
 
 }
